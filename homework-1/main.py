66340062ef90f0269a8ce06bc9adbb76157feca1
abduvohid75 <rustam75@list,ru>
2023-04-24 08:35:25 +0500

    проверка

--- homework-1/main.py
+++ homework-1/main.py
@@ -11,6 +11,8 @@ if __name__ == '__main__':
     Item.pay_rate = 0.8
     # применяем скидку
     item1.apply_discount()
+    #item2.apply_discount()
+
 
     print(item1.price)  # 8000.0
     print(item2.price)  # 20000
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+Item.pay_rate = 0.8
+
+def test_calculate_total_price():
+    assert item1.price * item1.quantity == 200000
+    assert item2.price * item2.quantity == 100000
+
+def test_apply_discount():
+    assert  item1.price * Item.pay_rate == 8000
+    assert  item2.price * Item.pay_rate == 16000
