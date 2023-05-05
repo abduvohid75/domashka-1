8a0a4437f84e398b9ccbbb531777e941dc03de36
abduvohid75 <rustam75@list,ru>
2023-05-05 16:12:46 +0500

    проверка

---
+++ src/phone.py
@@ -0,0 +1,14 @@
+from src.item import Item
+class Phone(Item):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim):
+        super().__init__(name, price, quantity)
+        self.number_of_sim = number_of_sim
+
+
+
+
+
+
+
+
+
